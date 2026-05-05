972be964a4b03030a00272bde64ff201cd503f7c
Saburi Bhambure <saburibhambure8@gmail.com>
2026-05-05 15:47:27 +0530

    prepared for the deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,6 +2,7 @@
 // const express = require("express")      // for type:commlyjs
 import cors from "cors"
 import dotenv from "dotenv"
+import path from "path"
 
 import notesRoutes from "../src/routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
@@ -12,10 +13,12 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5006;
+const __dirname = path.resolve()
 
 connectDB()
 
 //middleware : this middleware will parse JSON bodies
+if(process.env.NODE_ENV !== production){
 app.use(
     cors({
 origin : "http://localhost:5173"
@@ -23,9 +26,19 @@ origin : "http://localhost:5173"
 
 app.use(express.json());
 
+}
+
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
+
+if(procces.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend","dist", "index.html"))
+})
+}
 
 //What is an Endpoint
 //An endpoint is a combination of a URL + HTTP method that
